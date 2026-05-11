b32d90e644c961296fbdca1dc644ce749617ece7
Danielle Fortier <dtfortier@gmail.com>
2026-05-11 18:00:40 -0400

    Fix audience row card styling in Edit Dashboard header

--- src/pages/Customize.jsx
+++ src/pages/Customize.jsx
@@ -33,9 +33,10 @@ const AssignedIcon = () => (
   </svg>
 )
 
-const ChevronIcon = () => (
+const PencilIcon = () => (
   <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polyline points="6 9 12 15 18 9"/>
+    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
   </svg>
 )
 
@@ -78,7 +79,7 @@ function AudienceRow({ audience, onAudienceChange }) {
         onClick={() => setOpen(o => !o)}
         type="button"
       >
-        Edit assignment <ChevronIcon />
+        <PencilIcon /> Edit assignment
       </button>
 
       {open && (
